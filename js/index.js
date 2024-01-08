d2730648d0c9152b82b8e86a5df71eb780a7e5d9
tin-kri <113044805+tin-kri@users.noreply.github.com>
2024-01-08 10:14:23 +0100

    images rendered correctly

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
 const baseUrl = "http://cmc-ca.local/wp-json/wc/store/v1/products";
-const productContainer = document.querySelector(".products")
-
+const productContainer = document.querySelector(".products");
 
 async function getProducts(url) {
     const response = await fetch(url);
     const products = await response.json();
-    products.forEach(function (product) {
-        productContainer.innerHTML += `
-<div class="product">
-<h2>${product.name}</h2>
-<div class="product-image" style="background-image:url('${product.image}"
-</div>`
 
-    })
+    products.forEach(function (product) {
+        // Display product information
+        productContainer.innerHTML += `
+            <div class="product">
+                <h2>${product.name}</h2>
+            </div>`;
+
+        // Call the getImages function to display images
+        getImages(product);
+    });
+
     console.log(products);
 }
-doosdo
-dfjj
 
-getProducts(baseUrl); 
+async function getImages(product) {
+    // Check if the product has an image
+    const imageUrl = product.images.length > 0 ? product.images[0].src : 'path/to/placeholder-image.jpg';
+
+    // Append the image to the existing product container
+    productContainer.innerHTML += `
+        <img src="${imageUrl}" alt="${product.name}">
+    `;
+}
+
+getProducts(baseUrl);
