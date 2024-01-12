d4ab1b0681b3985f1fcfc4dbca4d42098532c822
tin-kri <113044805+tin-kri@users.noreply.github.com>
2024-01-12 15:05:44 +0100

    Lacking correct rendering on single product page

--- js/product.js
+++ js/product.js
@@ -1,16 +1,39 @@
-const singleProductContainer = document.querySelector(".product");
-const baseUrl = "http://cmc-ca.local/wp-json/wc/store/v1/products";
+const url = document.location;
 
-async function getProduct(url) {
-    const response = await fetch(url);
-    const product = await response.json();
 
-    console.log(product);
+const search = url.search;
 
-    product.innerHTML = `<h1>${product.name}</h1>
-    <div class="product.image"
-         style="background-image: url('${product_image}')"></div>
-    <div class="product.description">${product.description}</div>`;
+const params = new URLSearchParams(search);
+
+async function getSingleProduct(id) {
+    const url = `http://cmc-ca.local/wp-json/wc/store/v1/products/${id}`;
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+
+            return data;
+        } else {
+            throw new Error("There was an error with response.");
+        }
+    } catch (error) {
+        console.log('Error fetching single product');
+    }
 }
 
-getProduct(baseUrl);
+
+async function renderSingleProduct() {
+    const id = params.get('id');
+    const singleData = await getSingleProduct(id);
+    const singleProduct = document.getElementById("singleProduct");
+
+
+    singleProduct.innerHTML = JSON.stringify(singleData);
+
+
+}
+
+
+
+
+renderSingleProduct();
